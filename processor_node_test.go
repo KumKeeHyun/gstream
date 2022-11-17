ffe8a2d336f348e00ee96d3c26f66aba94861639
KumKeeHyun <kbzjung359@gmail.com>
2022-11-17 11:15:14 +0000

    add processor node, processor test

---
+++ processor_node_test.go
@@ -0,0 +1,79 @@
+package gstream
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type mockProcessorSupplier[V, VR any] struct {}
+
+var _ ProcessorSupplier[any, any] = &mockProcessorSupplier[any, any]{}
+
+func (*mockProcessorSupplier[V, VR]) Processor(_ ...Processor[VR]) Processor[V] {
+	return func(v V) {}
+}
+
+func newMockProcessorNode[V, VR any]() *processorNode[V, VR] {
+	return newProcessorNode[V, VR](&mockProcessorSupplier[V, VR]{})
+}
+
+func assertEqualPointer(t *testing.T, expected, actual any) {
+	assert.Equal(t, reflect.ValueOf(expected).Pointer(), reflect.ValueOf(actual).Pointer())
+}
+
+func TestAddChildStraight(t *testing.T) {
+	first := newMockProcessorNode[int, string]()
+	second := newMockProcessorNode[string, int]()
+	third := newMockProcessorNode[int, float64]()
+
+	addChild(first, second)
+	addChild(second, third)
+	
+	ffs := first.forwards()
+	assert.Equal(t, 1, len(ffs))
+	assertEqualPointer(t, second.processor, ffs[0])
+
+	sfs := second.forwards()
+	assert.Equal(t, 1, len(sfs))
+	assertEqualPointer(t, third.processor, sfs[0])
+
+	assert.Equal(t, 0, len(third.forwards()))	
+}
+
+func TestAddChildSplit(t *testing.T) {
+	first := newMockProcessorNode[int, string]()
+	second := newMockProcessorNode[string, int]()
+	third := newMockProcessorNode[string, float64]()
+
+	addChild(first, second)
+	addChild(first, third)
+	
+	ffs := first.forwards()
+	assert.Equal(t, 2, len(ffs))
+	assertEqualPointer(t, second.processor, ffs[0])
+	assertEqualPointer(t, third.processor, ffs[1])
+
+	assert.Equal(t, 0, len(second.forwards()))
+	assert.Equal(t, 0, len(third.forwards()))
+}
+
+func TestAddChildMerge(t *testing.T) {
+	first := newMockProcessorNode[int, string]()
+	second := newMockProcessorNode[int, string]()
+	third := newMockProcessorNode[string, float64]()
+
+	addChild(first, third)
+	addChild(second, third)
+	
+	ffs := first.forwards()
+	assert.Equal(t, 1, len(ffs))
+	assertEqualPointer(t, third.processor, ffs[0])
+
+	sfs := second.forwards()
+	assert.Equal(t, 1, len(sfs))
+	assertEqualPointer(t, third.processor, sfs[0])
+
+	assert.Equal(t, 0, len(third.forwards()))
+}
--- processor_test.go
+++ processor_test.go
@@ -1,6 +1,7 @@
 package gstream
 
 import (
+	"errors"
 	"strconv"
 	"testing"
 
@@ -22,6 +23,8 @@ func TestFallThroughProcessor(t *testing.T) {
 	assert.Equal(t, -1234, shouldBeEqualToV)
 }
 
+// -------------------------------
+
 func TestFilterProcessor(t *testing.T) {
 	var trueIfBiggerThan10 bool
 	p := newFilterProcessorSupplier(func(i int) bool {
@@ -38,13 +41,15 @@ func TestFilterProcessor(t *testing.T) {
 	assert.False(t, trueIfBiggerThan10)
 }
 
+// -------------------------------
+
 func TestMapProcessor(t *testing.T) {
 	var shouldBeItoa string
 	p := newMapProcessorSupplier(strconv.Itoa).
 		Processor(func(v string) {
 			shouldBeItoa = v
 		})
-	
+
 	p(10)
 	assert.Equal(t, strconv.Itoa(10), shouldBeItoa)
 	p(10000)
@@ -52,6 +57,8 @@ func TestMapProcessor(t *testing.T) {
 	p(-1234)
 	assert.Equal(t, strconv.Itoa(-1234), shouldBeItoa)
 }
+
+// -------------------------------
 
 func TestFlatMapProcessor(t *testing.T) {
 	var shouldBeInc []int
@@ -67,4 +74,90 @@ func TestFlatMapProcessor(t *testing.T) {
 
 	p(5)
 	assert.ElementsMatch(t, []int{0, 1, 2, 3, 4}, shouldBeInc)
+}
+
+// -------------------------------
+
+type mockKvstore struct{
+	store map[int]int
+}
+
+var _ KeyValueStore[int, int] = &mockKvstore{}
+
+func (kvs *mockKvstore) Get(key int) (int, error) {
+	v, ok := kvs.store[key]
+	if ok {
+		return v, nil
+	}
+	return 0, errors.New("mock error")
+}
+
+func (kvs *mockKvstore) Put(key int, value int) {
+	kvs.store[key] = value
+}
+
+func (*mockKvstore) Delete(key int) {
+	panic("unimplemented")
+}
+
+// -------------------------------
+
+func TestStreamToTableProcessor(t *testing.T) {
+	var shouldBeEqual KeyValue[int, Change[int]]
+
+	mockKvs := &mockKvstore{map[int]int{}}
+	p := newStreamToTableProcessorSupplier[int, int](mockKvs).
+		Processor(func(kv KeyValue[int, Change[int]]) {
+			shouldBeEqual = kv
+		})
+
+	p(NewKeyValue(1, 1))
+	assert.Equal(t, 1, shouldBeEqual.Key)
+	assert.Equal(t, 1, shouldBeEqual.Value.NewValue)
+
+	p(NewKeyValue(1, 2))
+	assert.Equal(t, 1, shouldBeEqual.Key)
+	assert.Equal(t, 1, shouldBeEqual.Value.OldValue)
+	assert.Equal(t, 2, shouldBeEqual.Value.NewValue)
+
+	p(NewKeyValue(1, 3))
+	assert.Equal(t, 1, shouldBeEqual.Key)
+	assert.Equal(t, 2, shouldBeEqual.Value.OldValue)
+	assert.Equal(t, 3, shouldBeEqual.Value.NewValue)
+}
+
+// -------------------------------
+
+func TestStreamTableJoinProcessor(t *testing.T) {
+	var shouldBeEqual KeyValue[int, int]
+
+	foundGetter := func(int) (int, error) { return 10, nil }
+	notFoundGetter := func(int) (int, error) { return 0, errors.New("mock error") }
+	joiner := func(v, vo int) int { return v + vo}
+	p := newStreamTableJoinProcessorSupplier(foundGetter, joiner).
+		Processor(func(kv KeyValue[int, int]) {
+			shouldBeEqual = kv
+		})
+	
+	p(NewKeyValue(1, 1))
+	assert.Equal(t, 1, shouldBeEqual.Key)
+	assert.Equal(t, 11, shouldBeEqual.Value)
+
+	p(NewKeyValue(2, 22))
+	assert.Equal(t, 2, shouldBeEqual.Key)
+	assert.Equal(t, 32, shouldBeEqual.Value)
+
+	var trueIfProcessed bool
+
+	p = newStreamTableJoinProcessorSupplier(notFoundGetter, joiner).
+		Processor(func(kv KeyValue[int, int]) {
+			trueIfProcessed = true
+		})
+	
+	p(NewKeyValue(1, 1))
+	assert.False(t, trueIfProcessed)
+
+	p(NewKeyValue(2, 1))
+	assert.False(t, trueIfProcessed)
+
 }
